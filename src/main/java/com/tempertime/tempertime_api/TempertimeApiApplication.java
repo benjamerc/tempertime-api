c8a0ab6b1803b577457d0d4d38cf3311e9483747
Benjamín Merchán <benjamerchan7@gmail.com>
2026-01-22 17:14:59 -0300

    Initial commit: project structure

---
+++ src/main/java/com/tempertime/tempertime_api/TempertimeApiApplication.java
@@ -0,0 +1,13 @@
+package com.tempertime.tempertime_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempertimeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempertimeApiApplication.class, args);
+	}
+
+}
