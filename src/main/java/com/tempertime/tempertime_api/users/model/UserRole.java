88a41fa3e97c77e4949deaa696ff5671b37b18f6
Benjamín Merchán <benjamerchan7@gmail.com>
2026-01-23 17:53:12 -0300

    feat(user): add UserRole enum

---
+++ src/main/java/com/tempertime/tempertime_api/users/model/UserRole.java
@@ -0,0 +1,9 @@
+package com.tempertime.tempertime_api.users.model;
+
+/**
+ * Enum of user roles.
+ * Currently only USER, can be extended with ADMIN or others.
+ */
+public enum UserRole {
+    USER
+}
