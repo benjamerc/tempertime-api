b9415669aceebcc7f22f114af3a4b659440a4972
Benjamín Merchán <benjamerchan7@gmail.com>
2026-01-24 23:41:21 -0300

    docs(user): update comments in UserRole enum

--- src/main/java/com/tempertime/tempertime_api/users/model/UserRole.java
+++ src/main/java/com/tempertime/tempertime_api/users/model/UserRole.java
@@ -1,9 +1,6 @@
 package com.tempertime.tempertime_api.users.model;
 
-/**
- * Enum of user roles.
- * Currently only USER, can be extended with ADMIN or others.
- */
+/** Application user roles */
 public enum UserRole {
     USER
 }
