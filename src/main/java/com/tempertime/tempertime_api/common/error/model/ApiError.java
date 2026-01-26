d717a07b23f28676c1a6911aa39c4c1d244eb2a9
Benjamín Merchán <benjamerchan7@gmail.com>
2026-01-26 19:22:23 -0300

    feat(common): add ApiError as standard API error response

---
+++ src/main/java/com/tempertime/tempertime_api/common/error/model/ApiError.java
@@ -0,0 +1,25 @@
+package com.tempertime.tempertime_api.common.error.model;
+
+import lombok.*;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+/** Standard API error response */
+@Getter
+@AllArgsConstructor
+@Builder
+public class ApiError {
+
+    private ErrorCode code;
+    private String message;
+
+    private String path;
+
+    @Builder.Default
+    private Instant timestamp = Instant.now();
+
+    @Builder.Default
+    private List<FieldError> details = new ArrayList<>();
+}
