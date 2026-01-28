3526ef750981d798af24a6d253b9ae01c605fc77
Benjamín Merchán <benjamerchan7@gmail.com>
2026-01-27 23:40:08 -0300

    feat(security): add SecurityAttributes enum

---
+++ src/main/java/com/tempertime/tempertime_api/security/util/SecurityAttributes.java
@@ -0,0 +1,8 @@
+package com.tempertime.tempertime_api.security.util;
+
+/** Request attribute keys for security errors */
+public enum SecurityAttributes {
+
+    /** Error code for auth/permission failures */
+    SECURITY_ERROR_CODE
+}
