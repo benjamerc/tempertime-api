f6baad7f4fb62740a253b312e7723043ef0af7ab
Benjamín Merchán <benjamerchan7@gmail.com>
2026-01-27 23:32:56 -0300

    feat(user): add UserNotFoundException

---
+++ src/main/java/com/tempertime/tempertime_api/users/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.tempertime.tempertime_api.users.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
