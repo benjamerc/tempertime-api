640081033964e9a65837f5a66694a5dc812b418d
Benjamín Merchán <benjamerchan7@gmail.com>
2026-01-27 23:30:26 -0300

    feat(common): add new error codes to ErrorCode enum and update comments

--- src/main/java/com/tempertime/tempertime_api/common/error/model/ErrorCode.java
+++ src/main/java/com/tempertime/tempertime_api/common/error/model/ErrorCode.java
@@ -3,17 +3,23 @@ package com.tempertime.tempertime_api.common.error.model;
 /** API error codes grouped by origin */
 public enum ErrorCode {
 
+    // User
+    INVALID_PASSWORD,
+
     // Auth
     BAD_CREDENTIALS,
     EMAIL_ALREADY_EXISTS,
 
     // Security
+    ACCESS_DENIED,
+    ACCESS_TOKEN_INVALID,
+    ACCESS_TOKEN_EXPIRED,
     REFRESH_TOKEN_EXPIRED,
     REFRESH_TOKEN_NOT_FOUND,
     REFRESH_TOKEN_REVOKED,
     EMAIL_NOT_FOUND,
 
-    // Common
+    // Generic
     VALIDATION_ERROR,
     INTERNAL_ERROR
 }
