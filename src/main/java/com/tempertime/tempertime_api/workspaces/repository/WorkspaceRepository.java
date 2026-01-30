57e08e5bd2dc5c116176ad99b667921f1c7599b4
Benjamín Merchán <benjamerchan7@gmail.com>
2026-01-30 19:53:37 -0300

    feat(workspace): add WorkspaceRepository

---
+++ src/main/java/com/tempertime/tempertime_api/workspaces/repository/WorkspaceRepository.java
@@ -0,0 +1,10 @@
+package com.tempertime.tempertime_api.workspaces.repository;
+
+import com.tempertime.tempertime_api.workspaces.model.Workspace;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/** Repository for Workspace: CRUD operations */
+@Repository
+public interface WorkspaceRepository extends JpaRepository<Workspace, Long> {
+}
