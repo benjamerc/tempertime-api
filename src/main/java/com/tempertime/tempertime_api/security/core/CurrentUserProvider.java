537effabe12ef2f2049fd88d7ba1140645512fe2
Benjamín Merchán <benjamerchan7@gmail.com>
2026-01-31 16:33:16 -0300

    feat(security): add CurrentUserProvider

---
+++ src/main/java/com/tempertime/tempertime_api/security/core/CurrentUserProvider.java
@@ -0,0 +1,18 @@
+package com.tempertime.tempertime_api.security.core;
+
+import com.tempertime.tempertime_api.users.model.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Component;
+
+/** Provides information about the currently authenticated user */
+@Component
+public class CurrentUserProvider {
+
+    public Long getUserId(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return userDetails.getUser().getId();
+    }
+
+    public User getUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return userDetails.getUser();
+    }
+}
