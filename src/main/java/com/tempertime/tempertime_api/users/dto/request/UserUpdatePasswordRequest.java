6f700dc8dba515e5872f6ffd258770e8601cba39
Benjamín Merchán <benjamerchan7@gmail.com>
2026-01-31 22:32:38 -0300

    refactor(user): add new validation to UserUpdatePasswordRequest

--- src/main/java/com/tempertime/tempertime_api/users/dto/request/UserUpdatePasswordRequest.java
+++ src/main/java/com/tempertime/tempertime_api/users/dto/request/UserUpdatePasswordRequest.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Size;
 public record UserUpdatePasswordRequest(
 
         @NotBlank(message = "Current password is required")
+        @Size(min = 8, max = 255, message = "Password must be between 8 and 255 characters")
         String currentPassword,
 
         @NotBlank(message = "New password is required")
