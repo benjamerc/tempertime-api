1d379c2e9dc5d5ad763d2b3578c20484f22509a8
Benjamín Merchán <benjamerchan7@gmail.com>
2026-01-31 22:31:13 -0300

    refactor(auth): add new validation to AuthLoginRequest

--- src/main/java/com/tempertime/tempertime_api/auth/dto/request/AuthLoginRequest.java
+++ src/main/java/com/tempertime/tempertime_api/auth/dto/request/AuthLoginRequest.java
@@ -2,14 +2,17 @@ package com.tempertime.tempertime_api.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 /** User login request */
 public record AuthLoginRequest(
 
         @NotBlank(message = "Email is required")
         @Email(message = "Invalid email format")
+        @Size(max = 255, message = "Email must be at most 255 characters")
         String email,
 
         @NotBlank(message = "Password is required")
+        @Size(max = 255, message = "Password must be at most 255 characters")
         String password
 ) {}
