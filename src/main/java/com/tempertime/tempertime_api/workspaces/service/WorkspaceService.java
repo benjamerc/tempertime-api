054d6cdd10f6a870f629e5c96d2acb586f505239
Benjamín Merchán <benjamerchan7@gmail.com>
2026-02-04 18:24:42 -0300

    refactor(workspace): update WorkspaceService return types
    Changed return types of updateWorkspace() and regenerateInviteCode() methods.

--- src/main/java/com/tempertime/tempertime_api/workspaces/service/WorkspaceService.java
+++ src/main/java/com/tempertime/tempertime_api/workspaces/service/WorkspaceService.java
@@ -9,7 +9,7 @@ import java.util.List;
 /** Workspace application service (user-scoped operations) */
 public interface WorkspaceService {
 
-    WorkspaceResponse createWorkspace(WorkspaceCreateRequest request, Long userId);
+    WorkspaceCreateResponse createWorkspace(WorkspaceCreateRequest request, Long userId);
 
     /** Workspaces the given user is a member of */
     List<WorkspaceListItemResponse> getUserWorkspaces(Long userId);
@@ -17,7 +17,7 @@ public interface WorkspaceService {
     /** Workspace details scoped to the given user */
     WorkspaceDetailResponse getWorkspaceById(Long workspaceId, Long userId);
 
-    WorkspaceResponse updateWorkspace(Long workspaceId, Long userId, WorkspaceUpdateRequest request);
+    WorkspaceUpdateResponse updateWorkspace(Long workspaceId, Long userId, WorkspaceUpdateRequest request);
 
     void archiveWorkspace(Long workspaceId, Long userId);
 
@@ -32,7 +32,7 @@ public interface WorkspaceService {
 
     WorkspaceCodeResponse disableInviteCode(Long workspaceId, Long userId);
 
-    WorkspaceCodeResponse regenerateInviteCode(Long workspaceId, Long userId);
+    WorkspaceCodeRegenerateResponse regenerateInviteCode(Long workspaceId, Long userId);
 
     /** Joins a workspace using a valid invitation code */
     WorkspaceJoinResponse joinWorkspace(String inviteCode, Long userId);
