4713f46842c7db3fd96b040a1b4aeba80b1a0c91
Benjamín Merchán <benjamerchan7@gmail.com>
2026-02-05 18:42:52 -0300

    feat(event): add EventMapper

---
+++ src/main/java/com/tempertime/tempertime_api/events/mapper/EventMapper.java
@@ -0,0 +1,11 @@
+package com.tempertime.tempertime_api.events.mapper;
+
+import com.tempertime.tempertime_api.events.dto.response.EventCreateResponse;
+import com.tempertime.tempertime_api.events.model.Event;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface EventMapper {
+
+    EventCreateResponse toEventCreateResponse(Event event);
+}
