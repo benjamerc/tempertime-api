7cf6c039c5c851c765340bf550d10bb33472c629
Benjamín Merchán <benjamerchan7@gmail.com>
2026-02-05 18:44:10 -0300

    feat(event): add EventController
    Added createEvent() endpoint.

---
+++ src/main/java/com/tempertime/tempertime_api/events/controller/EventController.java
@@ -0,0 +1,37 @@
+package com.tempertime.tempertime_api.events.controller;
+
+import com.tempertime.tempertime_api.events.dto.request.EventCreateRequest;
+import com.tempertime.tempertime_api.events.dto.response.EventCreateResponse;
+import com.tempertime.tempertime_api.events.service.EventService;
+import com.tempertime.tempertime_api.security.core.CurrentUserProvider;
+import com.tempertime.tempertime_api.security.core.CustomUserDetails;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/workspaces/{workspaceId}/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventService eventService;
+    private final CurrentUserProvider currentUserProvider;
+
+    @PostMapping
+    public ResponseEntity<EventCreateResponse> createEvent(
+            @PathVariable Long workspaceId,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @Valid @RequestBody EventCreateRequest request) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(eventService.createEvent(
+                        workspaceId,
+                        currentUserProvider.getUserId(userDetails),
+                        request
+                ));
+    }
+}
