3fe7a28197ed76a58b57c73e78972e49b4e48d52
Benjamín Merchán <benjamerchan7@gmail.com>
2026-02-06 22:54:49 -0300

    feat(event): add updateEvent() to EventService

--- src/main/java/com/tempertime/tempertime_api/events/service/EventService.java
+++ src/main/java/com/tempertime/tempertime_api/events/service/EventService.java
@@ -1,7 +1,9 @@
 package com.tempertime.tempertime_api.events.service;
 
 import com.tempertime.tempertime_api.events.dto.request.EventCreateRequest;
+import com.tempertime.tempertime_api.events.dto.request.EventUpdateRequest;
 import com.tempertime.tempertime_api.events.dto.response.EventCreateResponse;
+import com.tempertime.tempertime_api.events.dto.response.EventResponse;
 
 public interface EventService {
 
@@ -9,5 +11,12 @@ public interface EventService {
             Long workspaceId,
             Long userId,
             EventCreateRequest request
+    );
+
+    EventResponse updateEvent(
+            Long workspaceId,
+            Long eventId,
+            Long userId,
+            EventUpdateRequest request
     );
 }
