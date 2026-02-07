4bedb40ac7bbb8c2c496a8e3b3c63bc31880b52f
Benjamín Merchán <benjamerchan7@gmail.com>
2026-02-07 20:48:07 -0300

    feat(event): extend EventService with event access methods
    Added getEvents(), getEvent(), and deleteEvent().

--- src/main/java/com/tempertime/tempertime_api/events/service/EventService.java
+++ src/main/java/com/tempertime/tempertime_api/events/service/EventService.java
@@ -3,7 +3,10 @@ package com.tempertime.tempertime_api.events.service;
 import com.tempertime.tempertime_api.events.dto.request.EventCreateRequest;
 import com.tempertime.tempertime_api.events.dto.request.EventUpdateRequest;
 import com.tempertime.tempertime_api.events.dto.response.EventCreateResponse;
+import com.tempertime.tempertime_api.events.dto.response.EventListItemResponse;
 import com.tempertime.tempertime_api.events.dto.response.EventResponse;
+
+import java.util.List;
 
 public interface EventService {
 
@@ -18,5 +21,23 @@ public interface EventService {
             Long eventId,
             Long userId,
             EventUpdateRequest request
+    );
+
+    /** Retrieves all events from a workspace to which the user is assigned */
+    List<EventListItemResponse> getEvents(
+            Long workspaceId,
+            Long userId
+    );
+
+    EventResponse getEvent(
+            Long workspaceId,
+            Long eventId,
+            Long userId
+    );
+
+    void deleteEvent(
+            Long workspaceId,
+            Long eventId,
+            Long userId
     );
 }
