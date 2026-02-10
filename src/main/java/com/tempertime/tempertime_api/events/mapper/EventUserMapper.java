3f72acfb35f9ad121db05576fa7853032a60cd9b
Benjamín Merchán <benjamerchan7@gmail.com>
2026-02-09 23:37:00 -0300

    feat(event): add EventUserMapper
    Added toEventAssignedUserResponse().

---
+++ src/main/java/com/tempertime/tempertime_api/events/mapper/EventUserMapper.java
@@ -0,0 +1,16 @@
+package com.tempertime.tempertime_api.events.mapper;
+
+import com.tempertime.tempertime_api.events.dto.response.EventAssignedUserResponse;
+import com.tempertime.tempertime_api.events.model.EventUser;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+/** Maps EventUser relations to event-assigned user response DTOs */
+@Mapper(componentModel = "spring")
+public interface EventUserMapper {
+
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "user.firstName", target = "firstName")
+    @Mapping(source = "user.lastName", target = "lastName")
+    EventAssignedUserResponse toEventAssignedUserResponse(EventUser eventUser);
+}
