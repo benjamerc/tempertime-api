b0e4b42524e30e4baaabe301987ae4150544a59d
Benjamín Merchán <benjamerchan7@gmail.com>
2026-02-11 23:47:22 -0300

    feat(event): add EventPeriod

---
+++ src/main/java/com/tempertime/tempertime_api/events/query/model/EventPeriod.java
@@ -0,0 +1,10 @@
+package com.tempertime.tempertime_api.events.query.model;
+
+/** Time filter used to retrieve events within a specific period */
+public enum EventPeriod {
+
+    DAY,
+    WEEK,
+    MONTH,
+    ALL
+}
