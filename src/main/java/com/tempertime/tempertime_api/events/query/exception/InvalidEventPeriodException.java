33ed6da23edab86f4e5360854829e57b7964ec1d
Benjamín Merchán <benjamerchan7@gmail.com>
2026-02-11 23:49:09 -0300

    feat(event): add InvalidEventPeriodException

---
+++ src/main/java/com/tempertime/tempertime_api/events/query/exception/InvalidEventPeriodException.java
@@ -0,0 +1,11 @@
+package com.tempertime.tempertime_api.events.query.exception;
+
+public class InvalidEventPeriodException extends RuntimeException {
+    public InvalidEventPeriodException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidEventPeriodException(String message) {
+        super(message);
+    }
+}
