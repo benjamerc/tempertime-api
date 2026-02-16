26761defda0b95187ccd159d38cf4a28168bb0f1
Benjamín Merchán <benjamerchan7@gmail.com>
2026-02-16 18:59:47 -0300

    refactor(auth): implement ErrorCode in EmailAlreadyExistsException

--- src/main/java/com/tempertime/tempertime_api/auth/exception/EmailAlreadyExistsException.java
+++ src/main/java/com/tempertime/tempertime_api/auth/exception/EmailAlreadyExistsException.java
@@ -1,5 +1,7 @@
 package com.tempertime.tempertime_api.auth.exception;
 
+import com.tempertime.tempertime_api.common.error.model.ErrorCode;
+
 /**
  * Thrown when attempting to register a user with an email
  * that is already registered in the system.
@@ -7,6 +9,6 @@ package com.tempertime.tempertime_api.auth.exception;
 public class EmailAlreadyExistsException extends RuntimeException {
 
     public EmailAlreadyExistsException() {
-        super("Email already registered");
+        super(ErrorCode.EMAIL_ALREADY_EXISTS.getDefaultMessage());
     }
 }
