702d1b16b2f93893e8d81478f897e75b83552bba
Benjamín Merchán <benjamerchan7@gmail.com>
2026-02-20 16:18:57 -0300

    feat(workspace): add WorkspaceProperties

---
+++ src/main/java/com/tempertime/tempertime_api/workspaces/config/WorkspaceProperties.java
@@ -0,0 +1,18 @@
+package com.tempertime.tempertime_api.workspaces.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Configuration properties for workspace settings.
+ */
+@Component
+@ConfigurationProperties(prefix = "application.workspaces")
+@Getter
+@Setter
+public class WorkspaceProperties {
+
+    private int maxUsers;
+}
